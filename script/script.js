e8a0f44d73f8dec195b9296c4905c152127d8996
Renat <restspread@gmail.com>
2021-02-24 22:14:58 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const body = document.querySelector('body');
+
+for (let i = 0; i < 100; i++) {
+	const blocks = document.createElement('div');
+
+	blocks.classList.add('block');
+	body.appendChild(blocks);
+}
+
+function animateBlocks() {
+	anime({
+		targets: '.block',
+		translateX: function() {
+			return anime.random(-900, 900);
+		},
+		translateY: function() {
+			return anime.random(-500, 500);
+		},
+		scale: function() {
+			return anime.random(1, 5);
+		},
+		easing: 'easeInOutQuad',
+		autoplay: true,
+    duration: 5000,
+		delay: anime.stagger(10),
+		complete: animateBlocks
+	});
+}
+
+animateBlocks();
